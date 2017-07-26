a59b2a0488bbd3c86611c1b445fc73ea8ed06e76
Jaani Leskinen <jaani.leskinen@fraktio.fi>
2017-07-26 13:10:25 +0300

    d -> date

--- client/src/components/PeopleView/PeopleView.js
+++ client/src/components/PeopleView/PeopleView.js
@@ -64,7 +64,7 @@ export default class PeopleView extends Component {
         const weekNumber = nextProps.date.week();
         const weekNumberNow = moment().week();
 
-        if (this.props.d !== nextProps.d && week) {
+        if (this.props.date !== nextProps.date && week) {
             entryActions.setWeek(week);
         }
 
