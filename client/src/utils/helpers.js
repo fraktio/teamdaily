9206307313f8f6733837165ca4a7ea45608d42b2
Johan Ruokangas <johan@latehours.net>
2017-07-31 18:38:59 +0300

    Extract and simplify calculateWeekNumbers

--- client/src/utils/helpers.js
+++ client/src/utils/helpers.js
@@ -1,5 +1,22 @@
+import moment from 'moment';
+
 export const alphabeticalSort = (a, b) =>
   a.toLowerCase() !== b.toLowerCase() ? (a.toLowerCase() < b.toLowerCase() ? -1 : 1) : 0;
 export const getLatestEntry = entry => (entry.size > 0 ? entry.get(-1) : null);
 export const getEntryColor = entry => (entry ? entry.color : 'empty');
 export const doesFlaggedExist = entry => (entry ? entry.flagged : null);
+
+export const calculateWeekNumbers = amount => {
+  let weekNumbers = Array(amount + 1).fill({});
+
+  return weekNumbers
+    .map((_, i) => {
+      const date = moment().subtract(i, 'weeks');
+
+      return {
+        year: date.year(),
+        week: date.week(),
+      };
+    })
+    .reverse();
+};
