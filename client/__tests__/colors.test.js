8a65afa73dd34f2cbfed878deb779933c11806a4
Johan Ruokangas <johan@latehours.net>
2017-08-01 17:27:19 +0300

    Setup test

---
+++ client/__tests__/colors.test.js
@@ -0,0 +1,23 @@
+import colors from '../src/colors';
+
+describe('Color constants', () => {
+  test('Defined number of colors', () => {
+    expect(colors.length).toEqual(4);
+  });
+
+  test('Has color green', () => {
+    expect(colors.includes('green')).toEqual(true);
+  });
+
+  test('Has color yellow', () => {
+    expect(colors.includes('yellow')).toEqual(true);
+  });
+
+  test('Has color red', () => {
+    expect(colors.includes('red')).toEqual(true);
+  });
+
+  test('Has color blue', () => {
+    expect(colors.includes('blue')).toEqual(true);
+  });
+});
