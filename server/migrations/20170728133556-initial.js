0cad7697da47445991df88c22a12fc2a9ec7fe37
Jaani Leskinen <jaani.leskinen@fraktio.fi>
2017-08-08 14:11:50 +0300

    commit ja kotiin

--- server/migrations/20170728133556-initial.js
+++ server/migrations/20170728133556-initial.js
@@ -1,10 +1,10 @@
-'use strict';
+"use strict";
 
 var dbm;
 var type;
 var seed;
-var fs = require('fs');
-var path = require('path');
+var fs = require("fs");
+var path = require("path");
 var Promise;
 
 /**
@@ -19,35 +19,37 @@ exports.setup = function(options, seedLink) {
 };
 
 exports.up = function(db) {
-  var filePath = path.join(__dirname, 'sqls', '20170728133556-initial-up.sql');
-  return new Promise( function( resolve, reject ) {
-    fs.readFile(filePath, {encoding: 'utf-8'}, function(err,data){
+  var filePath = path.join(__dirname, "sqls", "20170728133556-initial-up.sql");
+  return new Promise(function(resolve, reject) {
+    fs.readFile(filePath, { encoding: "utf-8" }, function(err, data) {
       if (err) return reject(err);
-      console.log('received data: ' + data);
+      console.log("received data: " + data);
 
       resolve(data);
     });
-  })
-  .then(function(data) {
+  }).then(function(data) {
     return db.runSql(data);
   });
 };
 
 exports.down = function(db) {
-  var filePath = path.join(__dirname, 'sqls', '20170728133556-initial-down.sql');
-  return new Promise( function( resolve, reject ) {
-    fs.readFile(filePath, {encoding: 'utf-8'}, function(err,data){
+  var filePath = path.join(
+    __dirname,
+    "sqls",
+    "20170728133556-initial-down.sql"
+  );
+  return new Promise(function(resolve, reject) {
+    fs.readFile(filePath, { encoding: "utf-8" }, function(err, data) {
       if (err) return reject(err);
-      console.log('received data: ' + data);
+      console.log("received data: " + data);
 
       resolve(data);
     });
-  })
-  .then(function(data) {
+  }).then(function(data) {
     return db.runSql(data);
   });
 };
 
 exports._meta = {
-  "version": 1
+  version: 1
 };
